5cf25f24366260387c9ffd5d86f7cc9516dd948a
Kunal-kakkar06 <kunal.kakkar3010@gmail.com>
2026-04-03 18:23:27 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return the list of Requirements
+    '''
+    reqs=[]
+    with open(file_path) as file_obj:
+        reqs = file_obj.readlines()
+        reqs = [req.replace("\n","") for req in reqs]
+        if HYPEN_E_DOT in reqs:
+            reqs.remove(HYPEN_E_DOT)
+
+    return reqs
+ 
+setup(
+name = "mlProject",
+version="0.0.1",
+author="Kunal",
+packages = find_packages(),
+install_requires = get_requirements('requirements.txt')
+
+)
